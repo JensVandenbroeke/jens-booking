36aa19f571e03f7a31a88f7337f2c1dd565fcc37
JensVandenbroeke <jens.vandenbroeke1@gmail.com>
2026-05-29 00:58:17 +0200

    Fix double Router error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import BookingHome from './pages/BookingHome'
 import OpenConnectionFlow from './pages/OpenConnectionFlow'
 import CoachingCallFlow from './pages/CoachingCallFlow'
@@ -7,14 +7,12 @@ import AdminPage from './pages/AdminPage'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<BookingHome />} />
-        <Route path="/open-connection" element={<OpenConnectionFlow />} />
-        <Route path="/coaching" element={<CoachingCallFlow />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-        <Route path="/admin" element={<AdminPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<BookingHome />} />
+      <Route path="/open-connection" element={<OpenConnectionFlow />} />
+      <Route path="/coaching" element={<CoachingCallFlow />} />
+      <Route path="/confirmation" element={<Confirmation />} />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
   )
 }
